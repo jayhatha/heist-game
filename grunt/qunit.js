cdda7712eced500a66f1f4b9bbf80c13c28e468f
jayhatha <jay@jayhathaway.com>
2018-08-23 17:37:09 -0700

    still running tests

--- grunt/qunit.js
+++ grunt/qunit.js
@@ -4,7 +4,7 @@ module.exports = {
   all: {
     options: {
       urls: [
-        "http://127.0.0.1:8080/test.html"
+        "http://127.0.0.1:8080/test/test.html"
       ],
       timeout: 10000,
       screenshot: false
