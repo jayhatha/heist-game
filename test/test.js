29f2706b94af2ff9341ab21f5de8cf7ffde6909f
jayhatha <jay@jayhathaway.com>
2018-08-23 20:26:06 -0700

    consolidated some tests

--- test/test.js
+++ test/test.js
@@ -5,30 +5,22 @@ QUnit.test("hello test", (assert) => {
 QUnit.module("initializing game");
 QUnit.test("player has 5 credits", (assert) => {
   assert.equal(gameState.playerCash, 5, "should be 5");
-});
-QUnit.test("credit box displays 5 credits", (assert) => {
-  assert.equal($(".credits").text(), 5, "should be 5");
+  assert.equal($(".credits").text(), 5, "box should show 5");
 });
 QUnit.test("player has 4 actions", (assert) => {
-  assert.equal(gameState.playerClicks, 4, "should be 4");
-});
-QUnit.test("action box displays 4 actions", (assert) => {
-  assert.equal($(".clicks").text(), 4, "should be 4");
+  assert.equal(gameState.playerClicks, 4, "actions should be 4");
+  assert.equal($(".clicks").text(), 4, "box should show 4");
 });
 QUnit.test("it's the player's turn", (assert) => {
   assert.equal(gameState.playersTurn, true, "should be true");
 });
 QUnit.test("the player's score is 0", (assert) => {
   assert.equal(gameState.playerScore, 0, "should be 0");
-});
-QUnit.test("player's scorebox displays 0", (assert) => {
-  assert.equal($("#playerscore").text(), 0, "should be 0");
+  assert.equal($("#playerscore").text(), 0, "box should show 0");
 });
 QUnit.test("the bank's score is 0", (assert) => {
   assert.equal(gameState.corpScore, 0, "should be 0");
-});
-QUnit.test("bank's scorebox displays 0", (assert) => {
-  assert.equal($("#corpscore").text(), 0, "should be 0");
+  assert.equal($("#corpscore").text(), 0, "box should show 0");
 });
 QUnit.test("all banks have 0 trucks", (assert) => {
   assert.equal($(".bank").data().trucks, 0, "should be 0");
@@ -57,31 +49,27 @@ QUnit.module("resetting the game");
 QUnit.test("player has 5 credits", (assert) => {
   restartGame();
   assert.equal(gameState.playerCash, 5, "should be 5");
-});
-QUnit.test("credit box displays 5 credits", (assert) => {
-  assert.equal($(".credits").text(), 5, "should be 5");
+  assert.equal($(".credits").text(), 5, "box should show 5");
 });
 QUnit.test("player has 4 actions", (assert) => {
-  assert.equal(gameState.playerClicks, 4, "should be 4");
-});
-QUnit.test("action box displays 4 actions", (assert) => {
-  assert.equal($(".clicks").text(), 4, "should be 4");
+  assert.equal(gameState.playerClicks, 4, "actions should be 4");
+  assert.equal($(".clicks").text(), 4, "box should show 4");
 });
 QUnit.test("it's the player's turn", (assert) => {
   assert.equal(gameState.playersTurn, true, "should be true");
 });
 QUnit.test("the player's score is 0", (assert) => {
   assert.equal(gameState.playerScore, 0, "should be 0");
-});
-QUnit.test("player's scorebox displays 0", (assert) => {
-  assert.equal($("#playerscore").text(), 0, "should be 0");
+  assert.equal($("#playerscore").text(), 0, "box should show 0");
 });
 QUnit.test("the bank's score is 0", (assert) => {
   assert.equal(gameState.corpScore, 0, "should be 0");
-});
-QUnit.test("bank's scorebox displays 0", (assert) => {
-  assert.equal($("#corpscore").text(), 0, "should be 0");
+  assert.equal($("#corpscore").text(), 0, "box should show 0");
 });
 QUnit.test("all banks have 0 trucks", (assert) => {
   assert.equal($(".bank").data().trucks, 0, "should be 0");
+});
+QUnit.test("there are at least 5 security measures", (assert) => {
+  const result = $(".security").length > 4;
+  assert.ok(result, "should be true");
 });
